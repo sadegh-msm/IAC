9084d1f071c16d1f254f699a304f705472f843f6
mohamadsadegh mohamadi <mohammad.s.mohammadi@zoodfood.com>
2025-05-27 12:10:19 +0330

    feat: add terraform for creating 2 vms on arvancloud

---
+++ .terraform.lock.hcl
@@ -0,0 +1,23 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "terraform.arvancloud.ir/arvancloud/iaas" {
+  version = "0.8.0"
+  hashes = [
+    "h1:SKYouppvVpO0OY/bFyqQTM/2aPXqnt/427c/QjLmVKY=",
+    "zh:00d17802cc016b9348cda1b1821b3c7db233aabf5817f2b9944ac1d8b6473ba1",
+    "zh:0a56775512bfae8d2f9fe5177300997439d4fe0e5a441d0b382e3c6a73b5505e",
+    "zh:0fdec401546cba0594fb6629efbbb58a98846e1d8efa667e7535a94b7e15a279",
+    "zh:2e3eb6eced3bb0ec3fd607b05cb98a6cbff7be1aa3784eee88446198238c7fa7",
+    "zh:32cb62c503816593406b523dd96cf784ddab771790ebc55450db3b6136c4bceb",
+    "zh:4286ab4747aaadbc5dc273e4633dec783dd8d13ac2005950555be2386eb50cf2",
+    "zh:59142ca60e8898e855a6ed8d5599d181c6ca7ea1bec4950f900e13052fa85b90",
+    "zh:68cd1f6268d9cfeae64098a11b0d945fe7f734ea277ba92be2801793c2dc22f9",
+    "zh:6d92da100ff8b72d0221eb9c84965b52e304c38d14304238b1336d3be90daf96",
+    "zh:754cb4366238fe746730c2ec47b6d28820eb96e36f5dd567a1b91e9f62da9f05",
+    "zh:8923e70ad55aafa361771a88c4efee5a0989c3be034b49f12fd4e4660d7300c6",
+    "zh:ab3cfee34bfc72c9fe367edc0106250c3fecf8a26acca1abb9e6b2e6cf8d3331",
+    "zh:b2daa36576eeaaefa7b51ea48301578f0a1665f2ee29189ce979d938c9f2f63d",
+    "zh:e39319db4b5917db0cc99efcf5669f86ece9d8251832e8753df19096c32c8896",
+  ]
+}
---
+++ 2-vms.tf
@@ -0,0 +1,121 @@
+terraform {
+  required_providers {
+    arvan = {
+      source = "terraform.arvancloud.ir/arvancloud/iaas"
+    }
+  }
+}
+
+variable "ApiKey" {
+  type      = string
+  default   = "test"
+  sensitive = true
+}
+
+provider "arvan" {
+  api_key = var.ApiKey
+}
+
+variable "region" {
+  type        = string
+  default     = "ir-thr-ba1"
+}
+
+variable "chosen_distro_name" {
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "chosen_name" {
+  type        = string
+  default     = "24.04"
+}
+
+variable "chosen_plan_id" {
+  type        = string
+  default     = "g2-4-2-0"
+}
+
+data "arvan_security_groups" "default_security_groups" {
+  region = var.region
+}
+
+data "arvan_images" "terraform_image" {
+  region     = var.region
+  image_type = "distributions" // or one of: arvan, private
+}
+
+data "arvan_plans" "plan_list" {
+  region = var.region
+}
+
+locals {
+  chosen_image = try([for image in data.arvan_images.terraform_image.distributions : image
+  if image.distro_name == var.chosen_distro_name && image.name == var.chosen_name][0], null)
+  selected_plan = try([for plan in data.arvan_plans.plan_list.plans : plan if plan.id == var.chosen_plan_id][0], null)
+}
+
+data "arvan_networks" "terraform_network" {
+  region = var.region
+}
+
+# resource "arvan_network" "terraform_private_network" {
+#   region      = var.region
+#   description = "Terraform-created private network"
+#   name        = "tf_private_network"
+#   dhcp_range = {
+#     start = "10.255.255.19"
+#     end   = "10.255.255.150"
+#   }
+#   dns_servers    = ["8.8.8.8", "1.1.1.1"]
+#   enable_dhcp    = true
+#   enable_gateway = true
+#   cidr           = "10.255.255.0/24"
+#   gateway_ip     = "10.255.255.1"
+# }
+
+resource "arvan_abrak" "abrak_1" {
+  # depends_on = [arvan_network.terraform_private_network]
+  timeouts {
+    create = "1h30m"
+    update = "2h"
+    delete = "20m"
+    read   = "10m"
+  }
+  region       = var.region
+  name         = "abrak_1"
+  # ssh_key_name = "your-sshkey-name"
+  count        = 1
+  image_id     = local.chosen_image.id
+  flavor_id    = local.selected_plan.id
+  disk_size    = 25
+  # networks = [
+  #   {
+  #     network_id = arvan_network.terraform_private_network.network_id
+  #   }
+  # ]
+  security_groups = [data.arvan_security_groups.default_security_groups.groups[0].id]
+}
+
+resource "arvan_abrak" "abrak_2" {
+  # depends_on = [arvan_network.terraform_private_network]
+  timeouts {
+    create = "1h30m"
+    update = "2h"
+    delete = "20m"
+    read   = "10m"
+  }
+  region       = var.region
+  name         = "abrak_2"
+  # ssh_key_name = "your-sshkey-name"
+  count        = 1
+  image_id     = local.chosen_image.id
+  flavor_id    = local.selected_plan.id
+  disk_size    = 25
+  # networks = [
+  #   {
+  #     network_id = arvan_network.terraform_private_network.network_id
+  #   }
+  # ]
+  security_groups = [data.arvan_security_groups.default_security_groups.groups[0].id]
+}
